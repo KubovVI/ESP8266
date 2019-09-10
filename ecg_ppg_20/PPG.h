886c27e1fb3d1570bee9c3524d5e16fc49788170
KubovVI <49023394+KubovVI@users.noreply.github.com>
2019-09-10 11:06:09 +0300

    Add files via upload

---
+++ ecg_ppg_20/PPG.h
@@ -0,0 +1,8 @@
+// 4LF, 128HF Filtering 
+void getPPG(int16_t *Red, int16_t *IR); // Filtered data
+void getPPGraw(int32_t *Red, int32_t *IR); //Raw data
+//time interval >(16+2)ms
+char setupPPG(void); //1000sps, 16 samples average, Red+IR 
+void powerPPG(char power); //Sensor LEDs On/Off
+void setLeds(uint8_t Red, uint8_t IR); 
+
